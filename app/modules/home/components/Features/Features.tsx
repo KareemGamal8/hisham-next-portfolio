ac91b5b7947a6c7330247f0e2cee03641b5898a3
Kareem Gamal <kareemgmal7755@gmail.com>
2024-10-25 22:07:30 +0300

    Updates

--- app/modules/home/components/Features/Features.tsx
+++ app/modules/home/components/Features/Features.tsx
@@ -3,7 +3,7 @@ import AnimatedCounter from "@/app/design-system/components/AnimatedCounter";
 
 export default function Features() {
   return (
-    <div className="py-8">
+    <div className="py-16">
       <div className="container">
         <div className="grid sm:grid-cols-3 items-center grid-cols-1 justify-between gap-8">
           <AnimatedCounter
