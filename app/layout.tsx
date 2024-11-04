5da550117b0aa0862207cd518a2d5dbc0698f6ba
Kareem Gamal <kareemgmal7755@gmail.com>
2024-11-05 00:38:17 +0200

    Toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "./design-system/components/Header";
 import Footer from "./design-system/components/Footer";
 import AffixButton from "./design-system/buttons/AffixButton";
 import WhatsappButton from "./design-system/buttons/WhatsappButton";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./assets/fonts/Roboto-Black.ttf",
@@ -21,6 +22,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable}  antialiased flex flex-col min-h-[100vh]`}
       >
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 1500,
+            style: {
+              background: "#0f0714",
+              color: "white",
+              border: "3px solid #1c263d",
+              fontWeight: 600,
+            },
+          }}
+        />
         <Header />
         <AffixButton />
         <WhatsappButton />
